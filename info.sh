bd220dbda931580d820b2dfdf6b4595cb673e231
Mona Lisa <johndoe@example.com>
2026-07-16 10:30:53 +0200

    corsste

--- info.sh
+++ info.sh
@@ -8,7 +8,7 @@ echo "----PUT----"
 API="https://vercel.com/api"
 # TEAM="$VERCEL_ARTIFACTS_OWNER"
 echo "TEAM: $VERCEL_ARTIFACTS_OWNER"
-TEAM="team_SxcBaAjO2azHOPD9Shh60vn7"
+TEAM="team_sFsuZpbXNlvUFDRZWrluctaq"
 TOKEN="$VERCEL_ARTIFACTS_TOKEN"
 HASH="da2cbac07f32df91"
 echo $TEAM
